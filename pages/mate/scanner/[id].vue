f116c62e4741b634acdc1d2680d4cacea127f2a5
thespielplatz <informatics@gmx.net>
2025-01-03 19:05:19 +0100

    fix(scanner): refresh token timing on inventory item page
    RUN_PIPELINE

--- pages/mate/scanner/[id].vue
+++ pages/mate/scanner/[id].vue
@@ -78,6 +78,12 @@ const baseInfo = ref<OutputDtoType>(null)
 let code = ''
 const amount = ref(0)
 
+onBeforeMount(async () => {
+  if (!await $auth.isLoggedIn()) {
+    return await navigateTo('/')
+  }
+})
+
 onMounted(async () => {
   try {
     baseInfo.value = await $auth.$fetch('/api/scanner', {
